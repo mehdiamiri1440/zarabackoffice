1ef04e2ab2c495a619b51a1d8dcd1a2c22f59252
mehdi <knownmma@gmail.com>
2018-12-28 11:17:34 +0330

    changes for menu by mehdi amiri

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -14,18 +14,66 @@ class Menu extends Component {
       >
         <div
           style={{ width: "100%" }}
-          className="text-white  border-bottom text-center  p-2"
+          className="text-white d-flex position-relative justify-content-end openSubmenu border-bottom text-center  p-2"
         >
-          <span>مدیریت اسلاید ها</span>
+          <span className="text-center  align-middle">مدیریت اسلاید ها</span>
+          <div
+            id="submenu"
+            style={{ right: "100%", top: 0 }}
+            className="position-absolute w-100  submenu"
+          >
+            <div style={{}} className="align-middle p-2">
+              <div className="w-100 d-flex  align-items-center justify-content-end">
+                <span className="px-2">جدید ها</span>
+                <i className="far fa-newspaper" />
+              </div>
+            </div>
+          </div>
           <span>
-            <i className="fa fa-bars" />
+            <i className="fab fa-slideshare text-center px-2 align-middle" />
           </span>
         </div>
         <div
           style={{ width: "100%" }}
-          className="border-bottom text-white text-center p-2"
+          className="text-white d-flex justify-content-end openSubmenu position-relative  border-bottom text-center p-2"
         >
-          مدیریت دسته بندی ها
+          <span className="text-center  align-middle">مدیریت دسته بندی ها</span>
+          <div
+            id="submenu"
+            style={{ right: "100%", top: 0 }}
+            className="position-absolute w-100  submenu"
+          >
+            <div className="align-middle   p-2">
+              <div className=" w-100 d-flex align-items-center justify-content-end">
+                <span className="px-2">جدید ها</span>
+                <i className="far fa-newspaper" />
+              </div>
+            </div>
+          </div>
+          <span>
+            <i className="far fa-window-restore text-center px-2 align-middle" />
+          </span>
+        </div>
+        <div
+          style={{ width: "100%" }}
+          className="text-white d-flex justify-content-end openSubmenu position-relative  border-bottom text-center  p-2"
+        >
+          <span className="text-center  align-middle">مدیریت کاربران</span>
+          <div
+            id="submenu"
+            style={{ right: "100%", top: 0 }}
+            className="position-absolute w-100  submenu"
+          >
+            <div className="p-2">
+              <div className=" w-100 d-flex align-items-center justify-content-end">
+                <span className="px-2">جدید ها</span>
+                <i className="far fa-newspaper" />
+              </div>
+            </div>
+          </div>
+          <span>
+            <i className="fas fa-users-cog text-center px-2 align-middle" />
+          </span>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 import * as serviceWorker from "./serviceWorker";
 
